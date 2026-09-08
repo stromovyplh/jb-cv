efb5ebac02f929f00e5c3844fc718086a1645265
Jozef Balint <j.balint@enbw.com>
2026-09-08 14:59:02 +0100

    Add BaseButton component.

---
+++ app/components/BaseButton/BaseButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import type { IBaseButtonProps } from '@/app/types/components'
+import styles from './BaseButton.module.css'
+
+export const BaseButton = (props: IBaseButtonProps) => {
+  const classes = [styles['base-button'], props.classNames].filter(Boolean).join(' ')
+
+  return (
+    <button className={classes} {...props}>
+      {props.label}
+    </button>
+  )
+}
